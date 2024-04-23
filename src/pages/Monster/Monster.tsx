5bd8af2b17a457dbcbe21020cbe5a88a20ca8d1e
naathaliie <nathalielarsson@live.se>
2024-04-23 16:59:56 +0200

    animation

--- src/pages/Monster/Monster.tsx
+++ src/pages/Monster/Monster.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { monsterContext } from "../../components/context/MonsterContext";
 import { OneMonster } from "../../types/types";
